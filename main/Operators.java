d7d13e69d57edbc6b4fc417319ba80b08a42c5ae
sharmi147 <sharmim147@gmail.com>
2025-08-06 20:45:23 +0530

    Create Operators.java

---
+++ main/Operators.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Operators {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("Sum: "+(a+b)  );
+        System.out.println("Difference: "+ (a-b) ); 
+        System.out.println("Product: " +(a*b) ); 
+        System.out.println("Quotient: " +(a/b) ); 
+        System.out.println("Remainder: "+ (a%b) ); 
+
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
