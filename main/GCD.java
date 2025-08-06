69e7e311eaa5eec92ceac79d43d444bf4e89bbc0
sharmi147 <sharmim147@gmail.com>
2025-08-06 21:11:34 +0530

    Create GCD.java

---
+++ main/GCD.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class GCD {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+         Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int c=gcd(a,b);
+    System.out.println(c);
+    }
+    public static int gcd(int a,int b){
+     while(b!=0){ int temp=b;//t=18  t=12  6
+        b=a%b;//12%18=12b=12  18%12=6  12%6=0
+        a=temp;//a=18  12 
+    }return a;}
+}
